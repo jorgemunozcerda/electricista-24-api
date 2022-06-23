476de1fe7fdaea585f2ff37ad06ea74858b286e4
Mojx Mojx <omarmunozcerda@gmail.com>
2022-06-23 08:27:33 -0500

    testing mysql connection

--- app.js
+++ app.js
@@ -3,6 +3,24 @@ if (process.env.NODE_ENV !== 'production') {
 }
 const express = require('express');
 const app = express();
+const { Sequelize } = require('sequelize');
+
+const sequelize = new Sequelize(
+	process.env.DB_NAME,
+	process.env.DB_USERNAME,
+	process.env.DB_PASSWORD,
+	{
+		host: process.env.DB_HOST,
+		dialect: process.env.DB_DIALECT,
+	}
+);
+
+try {
+	await sequelize.authenticate();
+	console.log('Connection to database has been established successfully.');
+} catch (error) {
+	console.error('Unable to connect to the database:', error);
+}
 
 const cors = require('cors');
 
